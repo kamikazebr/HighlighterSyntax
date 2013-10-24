26c84041213c42e2bfbd979389592f2e2dda79a5
Felipe Novaes <windholyghost@gmail.com>
2013-10-24 15:13:34 -0200

    .

--- src/main/HighlighterSyntax.java
+++ src/main/HighlighterSyntax.java
@@ -45,6 +45,7 @@ import main.interpreter.AbstractSyntaxInterpreter;
  *	IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  *	CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  * 
+ *  
  * @author Kamikaze [Felipe Novaes F. da Rocha]
  * 
  */
